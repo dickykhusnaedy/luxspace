e4098447b4379b28bca80209aa9214f60a795b15
dickykhusnaedy98@gmail.com <dickykhusnaedy98@gmail.com>
2023-07-10 09:04:32 +0700

    setup admin area

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\FrontendController;
 use Illuminate\Support\Facades\Route;
 
@@ -22,12 +23,10 @@ Route::get('/detail/{slug}', [FrontendController::class, 'details'])->name('deta
 Route::get('/cart', [FrontendController::class, 'cart'])->name('cart');
 Route::get('/checkout/success', [FrontendController::class, 'success'])->name('checkout-success');
 
-Route::middleware([
-	'auth:sanctum',
-	config('jetstream.auth_session'),
-	'verified'
-])->group(function () {
-	Route::get('/dashboard', function () {
-		return view('dashboard');
-	})->name('dashboard');
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->name('dashboard.')->prefix('dashboard')->group(function () {
+	Route::get('/', [DashboardController::class, 'index'])->name('index');
+
+	Route::middleware(['admin'])->group(function () {
+		//
+	});
 });
