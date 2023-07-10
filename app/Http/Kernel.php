c05a8036f01dc5dbbceb52138197fd381c530733
dickykhusnaedy98@gmail.com <dickykhusnaedy98@gmail.com>
2023-07-10 10:57:24 +0700

    Fixed bugs middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,7 +2,7 @@
 
 namespace App\Http;
 
-use App\Http\Middleware\isAdmin;
+use App\Http\Middleware\IsAdmin;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -63,6 +63,6 @@ class Kernel extends HttpKernel
 		'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
 		'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
 		'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-		'admin' => isAdmin::class
+		'admin' => IsAdmin::class
 	];
 }
